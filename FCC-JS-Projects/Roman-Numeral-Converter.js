4f1a9f2628c125c845389c1e6d229976c1ffce5d
Majd Saleh <mjd.s.m@hotmail.com>
2019-05-22 19:08:32 +0300

    Roman-Numeral-Converter solution 2

--- FCC-JS-Projects/Roman-Numeral-Converter.js
+++ FCC-JS-Projects/Roman-Numeral-Converter.js
@@ -2,7 +2,7 @@
 
 // solution 1 : this solution doesn't pass all the test cases :) bcz it's stupid solution
 
-const ROMANS = {
+const ROMANS1 = {
     '1': 'I',
     '2': 'II',
     '3': 'III',
@@ -34,7 +34,7 @@ const ROMANS = {
 }
 
 
-function convertToRoman(num) {
+function convertToRoman1(num) {
 
     let num_arr = num.toString().split('');
     let roman = "";
@@ -55,4 +55,60 @@ function convertToRoman(num) {
     return solutin.join("");
 }
 
-convertToRoman(36);
+convertToRoman1(36);
+
+// solution 2: this is works with all test cases 
+const ROMANS = [
+    [1000, 'M'],
+    [500, 'D'],
+    [100, 'C'],
+    [50, 'L'],
+    [10, 'X'],
+    [5, 'V'],
+    [1, 'I']
+];
+
+function convertToRoman(num) {
+
+    let num_arr = num.toString().split('');
+    let solutin = [];
+
+    // To Break the number into Thousands, Hundreds, Tens and Ones
+    let num_parts = num_arr.map((i, index) => {
+        if (parseInt(i) > 1) {
+            let x = index + 1;
+            for (x; x < num_arr.length; x++) {
+                i += "0";
+            }
+            return i;
+        }
+    })
+    console.log(num_parts);
+
+    num_parts.forEach((i) => {
+
+        let NumIndexVal = parseInt(i);
+        let roman_num = "";
+
+        ROMANS.forEach((i) => {
+            while (NumIndexVal >= i[0]) {
+                NumIndexVal -= i[0];
+                roman_num += i[1];
+            }
+
+        });
+
+        if (roman_num) {
+            solutin.push(roman_num);
+        }
+
+
+    })
+
+
+
+    return solutin.join("");
+}
+
+convertToRoman(400);
+
